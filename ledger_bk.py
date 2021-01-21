1ab372bc62741979c0e46d1d76045f2b559e810f
Jiiihun <rjh7209@naver.com>
2021-01-21 14:35:50 +0900

    충돌 커밋

--- ledger_bk.py
+++ ledger_bk.py
@@ -15,6 +15,31 @@ def viewall():
     con.close()
     return rows
 
+<<<<<<< HEAD
+def search(name="",user="",password="",category=""):
+    con = sqlite3.connect("aledger.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM account WHERE name=? OR user=? OR password=? OR category=?",(name,user,password,category))
+    rows = cur.fetchall()
+    con.close()
+    return rows
+def add(name,user,password,category,cdate):
+    con = sqlite3.connect("aledger.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO account VALUES(NULL,?,?,?,?,?)",(name,user,password,category,cdate))
+    con.commit()
+    con.close()
+def update(id,name,user,password,category,cdate):
+    con = sqlite3.connect("aledger.db")
+    cur = con.cursor()
+    cur.execute("UPDATE account SET name=?,user=?,password=?,category=?,cdate=? WHERE id=?",(name,user,password,category,cdate,id))
+    con.commit()
+    con.close()
+def delete(id):
+    con = sqlite3.connect("aledger.db")
+    cur = con.cursor()
+    cur.execute("DELETE FROM account WHERE id=?",(id,))
+=======
 # def search(name="",user="",password="",category=""):
 #     con = sqlite3.connect("aledger.db")
 #     cur = con.cursor()
@@ -30,17 +55,18 @@ def add(account_num, name, password, money):
     con.commit()
     con.close()
     
- #def update(account_num,name,password):
- #    con = sqlite3.connect("aledger.db")
- #    cur = con.cursor()
- #    cur.execute("UPDATE account SET account_num=?, name=?,password=?,money=?",(account_num,name,password,money))
- #    con.commit()
- #    con.close()
+# def update(account_num,name,password):
+#     con = sqlite3.connect("aledger.db")
+#     cur = con.cursor()
+#     cur.execute("UPDATE account SET account_num=?, name=?,password=?,money=?",(account_num,name,password,money))
+#     con.commit()
+#     con.close()
 
 def delete(account_num):
     con = sqlite3.connect("aledger.db")
     cur = con.cursor()
     cur.execute("DELETE FROM account WHERE account_num=?",(account_num,))
+>>>>>>> 58bfc90fbdc00e34acc5bcc256aacd89a30c80ff
     con.commit()
     con.close()
 create()
